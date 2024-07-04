88833ebdfe8e8d43a98fa065ff867abac2398382
EnyaBu <enyabullard2@gmail.com>
2024-07-04 12:28:26 +0800

    add

--- 3d-react-app/src/App.js
+++ 3d-react-app/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import ThreeDModel from './ThreeDModel';
 function App() {
+
   return (
+    
     <div className="App">
-     
+    
       <ThreeDModel />
+      <p>Testing!</p>
       
     </div>
   );
--- 3d-react-app/src/ThreeDModel.js
+++ 3d-react-app/src/ThreeDModel.js
@@ -1,19 +1,27 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Html, OrbitControls, useGLTF, Preload } from '@react-three/drei';
+import { Popconfirm } from 'antd'
+import * as THREE from 'three'
+
 
 function Model() {
   const { scene } = useGLTF('store/ThriftstoreGLB.glb'); // Make sure to use the correct path to your 3D model
   return <primitive object={scene} />;
 }
 
-const ThreeDModel = () => {
+const ThreeDModel = ({onClick }) => {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [-3, 5, 10] }}>
       <Suspense fallback={null}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[0, 10, 5]} intensity={1.5} />
         <Model />
+        <Html scale={2} position={[-3, 5, 7]} transform occlude>
+          <Popconfirm title="Welcome to our shop! click around to explore details :)" onConfirm={onClick} okText="Yes" cancelText="No">
+            <a href="#">Click me to learn more!</a>
+          </Popconfirm>
+        </Html>
         <OrbitControls />
       </Suspense>
     </Canvas>
