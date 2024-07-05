55529a1e96481214ffbd52b63a72b5bace7a0c6c
EnyaBu <enyabullard2@gmail.com>
2024-07-05 11:49:34 +0800

    add loading page and zoom function

--- 3d-react-app/src/App.js
+++ 3d-react-app/src/App.js
@@ -1,16 +1,43 @@
 import './App.css';
 import ThreeDModel from './ThreeDModel';
+import React, { useState, useEffect } from 'react';
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader';
+
 function App() {
 
+  const [loading, setLoading] = useState(false);
+    
+      useEffect(() => {
+        // Simulate an API call
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false);
+        }, 8000);
+      }, []);
   return (
     
     <div className="App">
+    {
+    loading ? 
+    (
+      <ClimbingBoxLoader size={30} color={'#F37A24'} loading={loading} />
+    ) : 
     
+    (<div>
+
       <ThreeDModel />
-      <p>Testing!</p>
+      <h1>Welcome to our store!</h1>
+      <p> We focus on sustainable fashion. Click around our store to learn more!</p>
+
+    </div>
+    )
+      
+    };
       
     </div>
   );
 }
+
+
 
 export default App;
--- 3d-react-app/src/ThreeDModel.js
+++ 3d-react-app/src/ThreeDModel.js
@@ -1,8 +1,7 @@
 import React, { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Html, OrbitControls, useGLTF, Preload } from '@react-three/drei';
-import { Popconfirm } from 'antd'
-import * as THREE from 'three'
+import { Html, OrbitControls, useGLTF, Bounds, useBounds, ContactShadows } from '@react-three/drei';
+import { Popconfirm } from 'antd';
 
 
 function Model() {
@@ -10,23 +9,36 @@ function Model() {
   return <primitive object={scene} />;
 }
 
+//i disabled orbit controls so zooming on model works
 const ThreeDModel = ({onClick }) => {
   return (
     <Canvas camera={{ position: [-3, 5, 10] }}>
       <Suspense fallback={null}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[0, 10, 5]} intensity={1.5} />
-        <Model />
-        <Html scale={2} position={[-3, 5, 7]} transform occlude>
+        <Bounds fit clip observe margin={1.2}>
+          <SelectToZoom>
+            <Model />
+          </SelectToZoom>
+        </Bounds>
+        <Html scale={0.5} position={[-3, 5, 7]} transform occlude>
           <Popconfirm title="Welcome to our shop! click around to explore details :)" onConfirm={onClick} okText="Yes" cancelText="No">
             <a href="#">Click me to learn more!</a>
           </Popconfirm>
         </Html>
-        <OrbitControls />
       </Suspense>
     </Canvas>
   );
 };
+
+function SelectToZoom({ children }) {
+  const api = useBounds()
+  return (
+    <group onClick={(e) => (e.stopPropagation(), e.delta <= 2 && api.refresh(e.object).fit())} onPointerMissed={(e) => e.button === 0 && api.refresh().fit()}>
+      {children}
+    </group>
+  )
+}
 
 export default ThreeDModel;
 
