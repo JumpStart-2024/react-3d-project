9f289b3d36c6603b87b5ec4b66299f7481199dcf
EnyaBu <enyabullard2@gmail.com>
2024-07-24 09:48:56 +0800

    added flip card

--- 3d-react-app/src/Components/ModalComponent.js
+++ 3d-react-app/src/Components/ModalComponent.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { Modal } from 'antd';
-import { Card, Row} from 'antd';
+import React from "react";
+import { Modal } from "antd";
+import { Card, Row } from "antd";
+import ReactFlipCard from "reactjs-flip-card";
 
 const { Meta } = Card;
 
-const CustomModal = (
-  { 
-    title,
-    body,
-    isModalVisible, 
-    handleOk, 
-    handleCancel
-
-
-  }) => {
-  
-
+const CustomModal = ({
+  title,
+  body,
+  isModalVisible,
+  handleOk,
+  handleCancel,
+}) => {
   return (
     <>
       <Modal
@@ -25,36 +21,40 @@ const CustomModal = (
         onOk={handleOk}
         onCancel={handleCancel}
         isModalVisible={isModalVisible}
-        width={800}
+        width={1000}
         height={500}
         mask={true}
-        
       >
-
-
-        <Row justify='space-evenly'>
-
-          { body.map(element => 
-            {
-              return (
-                <Card
-                  hoverable
-                  style={{ width: 200 }}
-                  cover={<img src={element.image} />}
+        <Row justify="space-evenly">
+          {body.map((element) => {
+            return (
+              <ReactFlipCard
+                flipTrigger="onClick"
+                frontComponent={
+                  <Card
+                    hoverable
+                    style={{ width: 200 }}
+                    cover={<img src={element.image} />}
                   >
-                  <Meta title={element.title}/>
-                </Card>
-
-              )
-            })
-          }
-          
+                    <Meta title={element.title} />
+                  </Card>
+                }
+                backComponent={
+                  <Card
+                    hoverable
+                    style={{ width: 200 }}
+                    cover={<img src={element.image} />}
+                  >
+                    <Meta title="TEST" />
+                  </Card>
+                }
+              />
+            );
+          })}
         </Row>
       </Modal>
-    </> 
+    </>
   );
 };
 
 export default CustomModal;
-
-
