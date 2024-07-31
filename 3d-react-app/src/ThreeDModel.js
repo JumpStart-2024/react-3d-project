3df4610362bcbc25566b60239c0952c1a1aab8e3
EnyaBu <enyabullard2@gmail.com>
2024-07-31 22:46:27 +0800

    fixed reflection

--- 3d-react-app/src/ThreeDModel.js
+++ 3d-react-app/src/ThreeDModel.js
@@ -36,10 +36,15 @@ const Floor = () => {
     <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
       <planeGeometry args={[30, 30]} />
       <MeshReflectorMaterial
-        blur={[400, 100]}
-        resolution={1024}
+        blur={[300, 100]}
+        resolution={2048}
+        mixStrength={80}
         roughness={1}
-        mirror={0}
+        depthScale={1.2}
+        minDepthThreshold={0.4}
+        maxDepthThreshold={1.4}
+        color="white"
+        metalness={0.5}
       />
       <meshPhongMaterial color="grey" />
     </mesh>
